3a7b2b4dd5cf6b56e5cf4346f1aa2aea495c888f
yzr96 <eric.yu@usask.ca>
2021-07-20 14:49:52 -0700

    setup function

--- R/ado.R
+++ R/ado.R
@@ -1,3 +1,48 @@
-ado <- function () {
+#' The ADO index in COPD
+#'
+#' @param fev A number for forced expiratory volume in one second (FEV1) expressed as percentage
+#' @param mmrc A number for the dyspnea scale of modified Medical Research Council (mMRC)
+#' @param age A vector of patient's age
+#'
+#' @return the ADO index
+#' @export
+#'
+#' @examples
+#' ado(fev=65, mmrc=1, age=41)
+ado <- function (fev, mmrc, age) {
+  if (fev >= 65) {
+    obstruction <- 0
+  } else if (fev >= 36 & fev <= 64) {
+    obstruction <- 1
+  } else {
+    obstruction <- 2
+  }
 
+  if (mmrc >= 0 & mmrc <= 1) {
+    dyspnea <- 0
+  } else if (mmrc == 2) {
+    dyspnea <- 1
+  } else if (mmrc == 3) {
+    dyspnea <- 2
+  } else if (mmrc == 4) {
+    dyspnea <- 3
+  }
+
+  if (age >= 40 & age <= 49) {
+    age <- 0
+  } else if (age >= 50 & age <= 59) {
+    age <- 1
+  } else if (age >= 60 & age <= 69) {
+    age <- 2
+  } else if (age >= 70 & age <= 79) {
+    age <- 3
+  } else if (age >= 80 & age <= 89) {
+    age <- 4
+  } else if (age >= 90) {
+    age <- 5
+  }
+
+  ado_index <- age + obstruction + dyspnea
+  results <- ado_index
+  return(results)
 }
